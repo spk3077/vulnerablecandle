9c4524d1accf6377eff53f14e246141738b303ac
vandalieu <vandalieu@BSCapstone.v2.myguest.virtualbox.org>
2023-02-16 17:52:14 -0500

    Basic Component Layout, much more work needed!  Next step is Product Display and Auth Guard!

--- src/main/frontend/src/app/app-routing.module.ts
+++ src/main/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ShopComponent } from './shop/shop.component';
+import { ProductComponent } from './product/product.component';
+import { CollectionsComponent } from './collections/collections.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/shop', pathMatch: 'full' },
+  { path: 'shop', component: ShopComponent },
+  { path: 'product/:productid', component: ProductComponent },
+  { path: 'collections', component: CollectionsComponent },
+  { path: 'login', component: LoginComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
