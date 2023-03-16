2b1d356d68869db2c742b375ef21593b23ba2b02
hc6674 <hc6674@rit.edu>
2023-03-16 03:19:17 +0000

    fixed, the block will stay 30 min

--- backend/src/main/java/csec/vulnerable/security/BlockUserDetailsService.java
+++ backend/src/main/java/csec/vulnerable/security/BlockUserDetailsService.java
@@ -6,10 +6,14 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import csec.vulnerable.beans.User;
+import csec.vulnerable.dao.UserDao;
 
 @Service
 public class BlockUserDetailsService implements UserDetailsService {
@@ -20,60 +24,54 @@ public class BlockUserDetailsService implements UserDetailsService {
 
     private final Map<String, LocalDateTime> blockedUsers = new HashMap<>();
     private final Map<String, Integer> failedAttempts = new HashMap<>();
-
+    @Autowired
+	UserDao userDao;
+    
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         if (isBlocked(username)) {
-            throw new RuntimeException("User is blocked for 30 minutes due to multiple failed login attempts.");
+            throw new UsernameNotFoundException("User is blocked for 30 minutes due to multiple failed login attempts.");
         }
 
         // Load user from the data source
-        UserDetails userDetails = loadUserFromDataSource(username);
-
-        if (userDetails == null) {
-            throw new UsernameNotFoundException("User not found: " + username);
-        }
-
-        return userDetails;
-    }
-
-    private UserDetails loadUserFromDataSource(String username) {
-        // Implement your logic to load user details from the data source (e.g., database) here.
-        // For now, this method returns null as a placeholder.
-        return null;
-    }
+        User user = userDao.findByUsername(username);
+		if(user == null) {
+			throw new UsernameNotFoundException("User: " + username + " was not found in the database");
+		}
+		return user;
+	}
 
     public void increaseFailedAttempts(String username) {
         int attempts = failedAttempts.getOrDefault(username, 0);
         failedAttempts.put(username, attempts + 1);
-    
+
         LOGGER.info("Failed login attempts for user {}: {}", username, attempts + 1);
-    
+
         if (attempts + 1 >= MAX_FAILED_ATTEMPTS) {
             blockUser(username);
         }
     }
-    
+
     public boolean isBlocked(String username) {
         LocalDateTime blockedUntil = blockedUsers.get(username);
-    
+
         if (blockedUntil == null) {
             return false;
         }
-    
+
         boolean blocked = LocalDateTime.now().isBefore(blockedUntil);
-    
+
         LOGGER.info("User {} is{}blocked", username, blocked ? " " : " not ");
-    
+
         if (!blocked) {
             blockedUsers.remove(username);
             resetFailedAttempts(username);
         }
-    
+
         return blocked;
     }
 
-    private void resetFailedAttempts(String username) {
+    public void resetFailedAttempts(String username) {
         failedAttempts.remove(username);
     }
 
--- backend/src/main/java/csec/vulnerable/security/SecurityConfig.java
+++ backend/src/main/java/csec/vulnerable/security/SecurityConfig.java
@@ -3,14 +3,12 @@ package csec.vulnerable.security;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -42,17 +40,13 @@ public class SecurityConfig{
     LogoutSuccessHandlerImpl logoutSuccessHandlerImpl;
 
     @Autowired
-    @Qualifier("userDetailsServiceImpl")
-    UserDetailsService userDetailsService;
-
-    @Autowired
     BlockUserDetailsService blockUserDetailsService;
 
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
 
-        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setUserDetailsService(blockUserDetailsService);
         authProvider.setPasswordEncoder(passwordEncoder());
 
         return authProvider;
