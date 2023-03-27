4014ffbfbef775a4bb9143ec8edf317f6b53c7b6
Student <student@localhost.localdomain>
2023-03-27 12:40:17 -0400

    basic order functionality

---
+++ backend/src/main/sqlInserts/insertEcomOrders.sql
@@ -0,0 +1,3 @@
+use csec;
+INSERT INTO ecom_order (id, purchase_date, user_id) VALUES (1, '2020-06-22', (SELECT id from ecom_user WHERE id=1));
+INSERT INTO ecom_order_id (id, quantity, order_id, product_id) VALUES (1, 10, (SELECT id from ecom_order WHERE id=1), (SELECT id from ecom_product WHERE id=1));
