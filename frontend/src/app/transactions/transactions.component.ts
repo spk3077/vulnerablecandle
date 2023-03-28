ef694a4261babc29c9dd52edf9b53216e4feb733
Student <student@localhost.localdomain>
2023-03-28 18:15:10 -0400

    transactions page

--- frontend/src/app/transactions/transactions.component.ts
+++ frontend/src/app/transactions/transactions.component.ts
@@ -49,14 +49,14 @@ const ELEMENT_DATA: PeriodicElement[] = [
 
 /**
  * @title Basic use of `<table mat-table>`
-
-@Component({
-  selector: 'table-basic-example',
-  styleUrls: ['table-basic-example.css'],
-  templateUrl: 'table-basic-example.html',
-})
-export class TableBasicExample {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
-}
+ *
+ *@Component({
+ *  selector: 'table-basic-example',
+ *  styleUrls: ['table-basic-example.css'],
+ *  templateUrl: 'table-basic-example.html',
+ *})
+ *export class TableBasicExample {
+ *  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+ *  dataSource = ELEMENT_DATA;
+ *}
  */
