e3e238435e7073f7cde3c8fd79e38b2213cc20d2
Student <student@localhost.localdomain>
2023-03-28 16:14:40 -0400

    working with angular

--- frontend/src/app/transactions/transactions.component.ts
+++ frontend/src/app/transactions/transactions.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { OrderReceive } from '@app/_core/order';
+import { OrderService } from '@app/_services/order.service';
 
 @Component({
   selector: 'app-transactions',
@@ -6,5 +10,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./transactions.component.css']
 })
 export class TransactionsComponent {
+  orderItems!: OrderReceive[];
+  originalOrderItems!: OrderReceive[];
 
+  ngOnInit(): void {
+    this.getOrders();
+    this.originalOrderItems = this.orderItems;
+  }
+
+  // Retrive order to display
+  public getOrders(); void {
+    this.orderService.getOrders().subscribe(orderItems => this.orderItems = orderItems);
+  }
 }
