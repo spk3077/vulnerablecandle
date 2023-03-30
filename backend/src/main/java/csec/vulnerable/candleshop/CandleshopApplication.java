4df8e83c6e9565959e302d6ae45a1f54d207f37a
hc6674 <hc6674@rit.edu>
2023-03-30 23:03:42 +0000

    cors update2

--- backend/src/main/java/csec/vulnerable/candleshop/CandleshopApplication.java
+++ backend/src/main/java/csec/vulnerable/candleshop/CandleshopApplication.java
@@ -24,7 +24,7 @@ public class CandleshopApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:4200")
+						.allowedOrigins("http://localhost:4200","https://localhost:4200")
 						.allowedMethods("GET", "POST", "PUT", "DELETE")
 						.allowedHeaders("*")
 						.allowCredentials(true);
