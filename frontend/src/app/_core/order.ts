e3d95de8d1994595653e3949aa8befd3fc9df292
Student <student@localhost.localdomain>
2023-04-02 16:28:29 -0400

    updating orders

--- frontend/src/app/_core/order.ts
+++ frontend/src/app/_core/order.ts
@@ -3,6 +3,20 @@ export class OrderReceive {
     constructor(
         public id: number,
         public purchase_date: string,
-        public user_id: number,
+        public user_id: number
     ) {  }
 }
+
+//
+//    public id: number = 0;
+//    public purchase_date: string = "";
+//    public user_id: number = 0;
+
+//    public static forDisplay(id: number, purchase_date: string, user_id: number): OrderReceive {
+//    const cls = new OrderReceive();
+//    cls.id = id;
+//    cls.purchase_date = purchase_date;
+//    cls.user_id = user_id;
+//    return cls;
+//  }
+//}
