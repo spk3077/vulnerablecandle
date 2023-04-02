e5d1c49cbca7a7ccf3fe628c12802ab6b9c7f630
Student <student@localhost.localdomain>
2023-04-02 14:24:55 -0400

    finalizing transaction page

--- frontend/src/app/_core/order.ts
+++ frontend/src/app/_core/order.ts
@@ -1,12 +1,8 @@
 // CLASS FOR RECEIVING SHOPPING CART ITEM
 export class OrderReceive {
     constructor(
-<<<<<<< HEAD
-        public id: number
-=======
         public id: number,
         public purchase_date: string,
         public user_id: number,
->>>>>>> 9f354c0 (Finishing transaction implementation)
     ) {  }
 }
