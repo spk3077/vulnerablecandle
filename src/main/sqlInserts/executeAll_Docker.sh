acb20a4a1fd22247a8877bda5157195fd47fd505
Student <student@localhost.localdomain>
2023-04-26 22:22:22 -0400

    working on getting sql working

--- src/main/sqlInserts/executeAll_Docker.sh
+++ src/main/sqlInserts/executeAll_Docker.sh
@@ -7,8 +7,8 @@ pswd="csec77499981"
 #     do mysql -u root --password=$pswd < $file
 # done
 
-mysql -u root --password=$pswd < $SCRIPTS_DIR/insertEcomProduct.sql
-mysql -u root --password=$pswd < $SCRIPTS_DIR/insertEcomUserInfo.sql
-mysql -u root --password=$pswd < $SCRIPTS_DIR/insertEcomTags.sql
-mysql -u root --password=$pswd < $SCRIPTS_DIR/insertEcomProductReview.sql
-mysql -u root --password=$pswd < $SCRIPTS_DIR/insertEcomOrders.sql
+mysql -h 127.0.0.1 -u root --password=$pswd < $SCRIPTS_DIR/insertEcomProduct.sql
+mysql -h 127.0.0.1 -u root --password=$pswd < $SCRIPTS_DIR/insertEcomUserInfo.sql
+mysql -h 127.0.0.1 -u root --password=$pswd < $SCRIPTS_DIR/insertEcomTags.sql
+mysql -h 127.0.0.1 -u root --password=$pswd < $SCRIPTS_DIR/insertEcomProductReview.sql
+mysql -h 127.0.0.1 -u root --password=$pswd < $SCRIPTS_DIR/insertEcomOrders.sql
