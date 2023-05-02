0480bee4bfc4698c58dc27a339c46bb03113cbba
Student <student@localhost.localdomain>
2023-05-02 17:07:13 -0400

    Docker now adds sql inserts after backend is run

--- src/main/sqlInserts/executeAll_Docker.sh
+++ src/main/sqlInserts/executeAll_Docker.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 SCRIPTS_DIR=./temp
-#read -sp "Enter DB Password : " pswd
 pswd="csec77499981"
-# for file in $SCRIPTS_DIR/*.sql
-#     do mysql -u root --password=$pswd < $file
-# done
 
-mysql -h 127.0.0.1 -u root --password=$pswd < $SCRIPTS_DIR/insertEcomProduct.sql
-mysql -h 127.0.0.1 -u root --password=$pswd < $SCRIPTS_DIR/insertEcomUserInfo.sql
-mysql -h 127.0.0.1 -u root --password=$pswd < $SCRIPTS_DIR/insertEcomTags.sql
-mysql -h 127.0.0.1 -u root --password=$pswd < $SCRIPTS_DIR/insertEcomProductReview.sql
-mysql -h 127.0.0.1 -u root --password=$pswd < $SCRIPTS_DIR/insertEcomOrders.sql
+#waiting for mysql to boot
+sleep 1m
+sleep 30s
+
+mysql -h 172.28.1.2 -u root --password=$pswd < $SCRIPTS_DIR/insertEcomProduct.sql
+mysql -h 172.28.1.2 -u root --password=$pswd < $SCRIPTS_DIR/insertEcomUserInfo.sql
+mysql -h 172.28.1.2 -u root --password=$pswd < $SCRIPTS_DIR/insertEcomTags.sql
+mysql -h 172.28.1.2 -u root --password=$pswd < $SCRIPTS_DIR/insertEcomProductReview.sql
+mysql -h 172.28.1.2 -u root --password=$pswd < $SCRIPTS_DIR/insertEcomOrders.sql
